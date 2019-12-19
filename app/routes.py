dd2edd651764d7ef3c0ef8d396163070d1a49225
Apostola <allex.post26@gmail.com>
2019-12-19 22:06:28 +0200

    style

--- app/routes.py
+++ app/routes.py
@@ -7,4 +7,5 @@ from app import app
 @app.route('/mirror')
 def index():
     user = {'username': 'Rares'}
-    return render_template('mirror.html', user=user)
+    font_url = 'https://fonts.googleapis.com/css?family=Montserrat&display=swap'
+    return render_template('mirror.html', user=user, font_url=font_url)
