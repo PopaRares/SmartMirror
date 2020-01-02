6d9da87c5fb073c6c2c7fa8681f620ec26247973
Rares Popa <raresp98@gmail.com>
2020-01-02 12:46:48 +0200

    Initialised database. Added user model.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 import threading
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.static_folder = 'static'
 
-from app import routes
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../DB/mirror.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+from app import routes, user
