4b9a82bab69c438de880547ba9e7c67ec2e8dd35
Apostola <allex.post26@gmail.com>
2020-01-11 23:54:55 +0200

    integrating Calendar

--- mirror.py
+++ mirror.py
@@ -2,6 +2,8 @@ import threading
 
 from app import app
 from face_detection import faces
+from apiclient import discovery
+from google_auth_oauthlib.flow import InstalledAppFlow
 
 
 class Mirror:
@@ -9,11 +11,18 @@ class Mirror:
     has_face = False
 
     def __init__(self):
-        test = 5
         self.mirror_web = threading.Thread(target=app.run, daemon=True, name='web')
         self.face_detect = threading.Thread(target=faces.detect, name='face_detect')
         self.mirror_web.start()
         self.face_detect.start()
 
+        scopes = ["https://www.googleapis.com/auth/calendar.readonly"]
+
+        flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", scopes=scopes)
+        flow.run_console()
+
 
 m = Mirror()
+
+
+
